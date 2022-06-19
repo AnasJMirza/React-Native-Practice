7b6d2c5c0e95cea0a608124e988586c5e2902804
AnasJMirza <ajmhimself@gmail.com>
2022-06-20 03:36:19 +0500

    let's make a todolist

---
+++ TodoList/src/screens/signinScreen/SigninScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text>SigninScreen</Text>
+    </View>
+  )
+}
+
+export default SigninScreen
