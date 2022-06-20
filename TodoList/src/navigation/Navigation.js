1c1fc22ac70e6f28410174543782634c72b8ee75
AnasJMirza <ajmhimself@gmail.com>
2022-06-20 23:26:46 +0500

    remove unused code

--- TodoList/src/navigation/Navigation.js
+++ TodoList/src/navigation/Navigation.js
@@ -1,8 +1,7 @@
-// import { View, Text } from 'react-native'
 import React from 'react'
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from '../screens/homeScreen/HomeScreen';
 import SplashScreen from '../screens/splashScreen/SplashScreen';
 
